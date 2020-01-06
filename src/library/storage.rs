ce9a8d36948e441d97efbb2404abffa29243342b
victor.fedorenko <victor.fedorenko@mgid.com>
2020-01-06 16:54:13 +0200

    Added missed files

---
+++ src/library/storage.rs
@@ -0,0 +1,37 @@
+use clipboard::{ClipboardContext, ClipboardProvider};
+use std::error::Error;
+use std::process::{Command, Stdio};
+
+pub trait Storage {
+    fn get(&self) -> Result<String, Box<dyn Error>>;
+}
+
+pub struct Clipboard;
+
+impl Storage for Clipboard {
+    fn get(&self) -> Result<String, Box<dyn Error>> {
+        let mut context: ClipboardContext = ClipboardProvider::new().unwrap();
+        context.get_contents()
+    }
+}
+
+pub struct Selected;
+
+impl Storage for Selected {
+    fn get(&self) -> Result<String, Box<dyn Error>> {
+        Command::new("xsel")
+            .arg("-o")
+            .stdout(Stdio::null())
+            .output()
+            .map(|output| String::from_utf8_lossy(&output.stdout).to_string())
+            .map_err(|err| err.into())
+    }
+}
+
+pub fn get_storage(use_selected: &bool) -> Box<dyn Storage> {
+    if *use_selected {
+        Box::new(Selected)
+    } else {
+        Box::new(Clipboard)
+    }
+}
